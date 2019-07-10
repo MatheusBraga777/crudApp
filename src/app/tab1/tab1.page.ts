f726f268ef1439cddda4b5b231a1f7fe88a4b89f
MatheusBraga777 <matheusamaralbraga5@gmail.com>
2019-07-10 12:05:46 -0300

    ionic
    mobile

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AlunoService } from '../services/aluno.service';
+import { Router } from '@angular/router';
+import { Aluno } from '../models/aluno.models';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  
+  formAluno: FormGroup;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder, 
+              private alunoService: AlunoService,
+              private router: Router) {}
+  
+  ngOnInit(): void {
+    this.formAluno = this.formBuilder.group({
+      nomeAluno: [
+        '', // parametro responsável pelo valor(conteúdo do campo), caso adicione um valor será exibido no input
+        [
+          Validators.required, // validação de campo requerido
+          Validators.minLength(4), // validação de valor minimo de caracteres
+          Validators.maxLength(150), // validação de valor maximo de caracteres
+          Validators.pattern(/^[a-zA-Z]+$/) // validação de tipo de caracteres (somente aceita letras minusculas e maiusculas)
+        ]
+      ],
+      matriculaAluno: [
+        '', 
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(5),
+          Validators.pattern(/^[0-9]+$/) // validação de tipo de caracteres (somente aceita números)
+        ]
+      ],
+      emailAluno: [
+        '', 
+        [
+          Validators.required,
+          Validators.email // validação de email
+        ]
+      ]
+    });
+  }
+}
 
+adcAluno() {
+  const novoAluno = this.formAluno.getRawValue() as Aluno;
+
+  this.alunoService
+  .addAluno(novoAluno)
+  .subscribe(
+    () => this.router.navigateByUrl('tab3'),
+  )
 }
